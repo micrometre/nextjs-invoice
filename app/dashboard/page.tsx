111924363d3c71481c3f2a59a5eb9bb89872b9e9
Henok Wehibe <henokwehibe@gmail.com>
2025-09-25 09:55:46 +0100

    sorting data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
+import { fetchRevenue } from '../../lib/data';
+
+
+export default async function Page() {
+    const revenue = await fetchRevenue();
+    console.log(revenue);
   return <p>Dashboard Page</p>;
 }
---
+++ lib/data.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '../app/generated/prisma';
+import {
+  Revenue,
+} from './definitions';
+
+const prisma = new PrismaClient();
+
+export async function fetchRevenue() {
+  try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const data = await prisma.revenue.findMany();
+
+    console.log('Data fetch completed after 3 seconds.');
+
+    return data;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch revenue data.');
+  }
+}
+
+
+const ITEMS_PER_PAGE = 6;
+
